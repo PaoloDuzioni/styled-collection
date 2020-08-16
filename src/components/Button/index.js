e377b5503959b0038f61b9e12145e0ffdbaae3eb
paolo duzioni <paolo@iMac-di-paolo.local>
2020-08-16 12:27:20 +0200

    initial project setup

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.a`
+    display: inline-block;
+    padding: 0.7rem 1.5rem;
+    text-decoration: none;
+    color: #fff;
+    background: #26a69a;
+    border: 0;
+    border-radius: 4px;
+`;
+
+export default Button;
